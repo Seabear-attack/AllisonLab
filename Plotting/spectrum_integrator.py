b84f7922d3ca72a0bf74e9b455b996d483f83f02
Mike <wahlmike147@gmail.com>
2023-09-22 14:47:13 -0400

    2.2 ADHNLF spectrum_integrator.py
    Signed-off-by: Mike <wahlmike147@gmail.com>

--- Plotting/spectrum_integrator.py
+++ Plotting/spectrum_integrator.py
@@ -6,27 +6,49 @@ from pathlib import Path
 
 if __name__ == "__main__":
     directorypath = Path(r'C:\Users\wahlm\Documents\School\Research\Allison\Tunable Pump\Pulse Optimization and '
-                             r'Spectrum Generation\9-21-23 Tunable seed spectrum optimization\Yokogawa OSA')
+                         r'Spectrum Generation\9-21-23 Tunable seed spectrum optimization\OAP\ADHNLF')
     raw_data = readFromFiles(directorypath)
-    labels = ('1s',
-              '0s')
+    labels = ('0s, 4A',
+              '0s, 3.75A',
+              '0s, 3.5A',
+              '0s, 3.25A',
+              '0s, 3A',
+              '1s, 4A',
+              '1s, 3.75A',
+              '1s, 3.5A',
+              '1s, 3.25A',
+              '1s, 3A',
+              '1s, 2.75A'
+              )
 
-    powers_mW = (208, 204)
+    powers_mW = (201,
+                 194,
+                 185,
+                 176,
+                 169,
+                 196,
+                 189,
+                 181,
+                 173,
+                 166,
+                 155)
 
     data = [OSAData(dat, ('nm', 'dBm'), labels[i], powers_mW[i], frep_MHz=60.5) for i, dat in enumerate(raw_data)]
 
     # Create a figure and axis object using matplotlib
     fig, ax = plt.subplots(figsize=(20, 8))
 
-    for dat in data:
+    for i, dat in enumerate(data):
         dat.y_axis_units = 'mW'
-        ax.plot(dat.x_axis_data, dat.y_axis_data, label=dat.label)
+        ax.plot(dat.x_axis_data, dat.y_axis_data, label=f'#{i}: {dat.label}')
     # Add axis labels and a legend
     ax.set_xlabel(f'Wavelength ({data[0].x_axis_units})')
     ax.set_ylabel(f'Spectral Power ({data[0].y_axis_units})')
     # ax.set_ylim([-60, 0])
     ax.legend()
-    # ax.set_title()
+    ax.set_title('ADHNLF Spectra')
+    plt.vlines(1300, min(data[0].y_axis_data), max(data[0].y_axis_data), colors='k')
+    plt.vlines(1800, min(data[0].y_axis_data), max(data[0].y_axis_data), colors='k')
 
     # Display the plot
     fig.canvas.manager.window.showMaximized()  # toggle fullscreen mode
