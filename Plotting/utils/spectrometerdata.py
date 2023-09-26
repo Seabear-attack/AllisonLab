a1931881a3090a5b8f1a5cc93c99d14de74bf285
Mike <wahlmike147@gmail.com>
2023-09-26 10:01:41 -0400

    Update spectrometerdata.py
    Add handling for negative linear inputs
    
    Signed-off-by: Mike <wahlmike147@gmail.com>

--- Plotting/utils/spectrometerdata.py
+++ Plotting/utils/spectrometerdata.py
@@ -71,14 +71,14 @@ class OSAData:
     def __init__(self, data, axis_units, data_label, spectrum_power_mW, frep_MHz=60.5):
         self._x_axis_units = axis_units[0]
         self._y_axis_units = axis_units[1]
+        self.__data_is_log = (self.y_axis_units[:2] == 'dB')
         self.x_axis_data = data[:, 0]
         self.y_axis_data = data[:, 1]
         self.total_power_mW = spectrum_power_mW
         self.label = data_label
         self._frep_MHz = frep_MHz
         self._pulse_energy_nJ = self.total_power_mW / self._frep_MHz
-
-        self.y_axis_units = axis_units[1]
+        self.__normalize()
 
     @property
     def x_axis_units(self):
@@ -95,54 +95,77 @@ class OSAData:
 
     @y_axis_units.setter
     def y_axis_units(self, units):
-        if self._y_axis_units[:2] == 'dB':
-            self.y_axis_data = np.power(10, self.y_axis_data / 10)
         self._y_axis_units = units
         self.__normalize()
+
+    @property
+    def y_axis_data(self):
+        return self._y_axis_data
+
+    @y_axis_data.setter
+    def y_axis_data(self, data):
+        if self.__data_is_log:
+            self._y_axis_data = np.nan_to_num(data, nan=-np.inf)
+        else:
+            self._y_axis_data = data
+            self._y_axis_data[self._y_axis_data < 0] = 0
 
     def integral(self, lower_bound=None, upper_bound=None):
         if lower_bound is None:
             lower_bound = min(self.x_axis_data)
         if upper_bound is None:
             upper_bound = max(self.x_axis_data)
-        in_bounds = np.where(np.logical_and(lower_bound <= self.x_axis_data,    self.x_axis_data <= upper_bound))
+        in_bounds = np.where(np.logical_and(lower_bound <= self.x_axis_data, self.x_axis_data <= upper_bound))
         y_data_subsection = self.y_axis_data[in_bounds]
-        #TODO fix units handling
-        if self.y_axis_units[:2] == 'dB':
+        if self.__data_is_log:
             return sum(np.power(10, y_data_subsection / 10))
         else:
             return sum(y_data_subsection)
 
     def __normalize(self):
-        integral = sum(self.y_axis_data)
-        delta_lambda = self.x_axis_data[1] - self.x_axis_data[0]
+        integral = self.integral()
+        delta_lambda = (self.x_axis_data[-1] - self.x_axis_data[0]) / (len(self.x_axis_data) - 1)
+        if self.__data_is_log:
+            linear_y_data = np.power(10, self.y_axis_data / 10)
 
+        else:
+            linear_y_data = self.y_axis_data
+
+        # TODO
         if self._y_axis_units == 'mW':
-            self.y_axis_data = self.y_axis_data * self.total_power_mW / integral
+            self.__data_is_log = False
+            self.y_axis_data = linear_y_data * self.total_power_mW / integral
 
         elif self._y_axis_units == 'mW/nm':
-            self.y_axis_data = self.y_axis_data * self.total_power_mW / integral / delta_lambda
+            self.__data_is_log = False
+            self.y_axis_data = linear_y_data * self.total_power_mW / integral / delta_lambda
 
         elif self._y_axis_units == 'dBm':
-            self.y_axis_data = 10 * np.log10(self.y_axis_data * self.total_power_mW / integral)
+            self.__data_is_log = True
+            self.y_axis_data = 10 * np.log10(linear_y_data * self.total_power_mW / integral)
 
         elif self._y_axis_units == 'dBm/nm':
-            self.y_axis_data = 10 * np.log10(self.y_axis_data * self.total_power_mW / integral / delta_lambda)
+            self.__data_is_log = True
+            self.y_axis_data = 10 * np.log10(linear_y_data * self.total_power_mW / integral / delta_lambda)
 
         elif self._y_axis_units == 'nJ':
-            self.y_axis_data = self.y_axis_data * self._pulse_energy_nJ / integral
+            self.__data_is_log = False
+            self.y_axis_data = linear_y_data * self._pulse_energy_nJ / integral
 
         elif self._y_axis_units == 'dBnJ':
-            self.y_axis_data = 10 * np.log10(self.y_axis_data * self._pulse_energy_nJ / integral)
+            self.__data_is_log = True
+            self.y_axis_data = 10 * np.log10(linear_y_data * self._pulse_energy_nJ / integral)
 
         elif self._y_axis_units == 'nJ/nm':
-            self.y_axis_data = self.y_axis_data * self._pulse_energy_nJ / integral / delta_lambda
+            self.__data_is_log = False
+            self.y_axis_data = linear_y_data * self._pulse_energy_nJ / integral / delta_lambda
 
         elif self._y_axis_units == 'dBnJ/nm':
-            self.y_axis_data = 10 * np.log10(self.y_axis_data * self._pulse_energy_nJ / integral / delta_lambda)
+            self.__data_is_log = True
+            self.y_axis_data = 10 * np.log10(linear_y_data * self._pulse_energy_nJ / integral / delta_lambda)
 
 
-def readFromFiles(path, pattern = '*.csv', skip_header=40):
+def readFromFiles(path, pattern='*.csv', skip_header=40):
     data_list = []
     files = path.glob(pattern)
     filepaths = natsorted([file for file in files], key=lambda f: str(f.name))
