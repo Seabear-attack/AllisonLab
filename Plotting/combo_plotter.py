1871cbaaf707fec7e81fbd62208ca67d060e4fc3
Mike <wahlmike147@gmail.com>
2023-09-27 20:06:29 -0400

    Paper scope trace figure ver. 1
    Signed-off-by: Mike <wahlmike147@gmail.com>

--- Plotting/combo_plotter.py
+++ Plotting/combo_plotter.py
@@ -6,63 +6,69 @@ import numpy as np
 from pathlib import Path
 
 if __name__ == "__main__":
-    save_fig = False
-    filename = 'post_EDFA_scope+spectrum'
+    save_fig = True
+    filename = 'polarization_scope_traces.eps'
+    savepath = Path(r'C:\Users\wahlm\Documents\School\Research\Allison\Papers\tunable-pump-seed')
+
+
+    # Pyplot options
+    fig, ax = plt.subplots(1, 2, figsize=(12, 8))
+    title_text_size = 16
+    axes_text_size = 14
+    legend_text_size = 12
+
+    # Begin plot of post-EDFA pulses
     directorypath = Path(r'C:\Users\wahlm\Documents\School\Research\Allison\Tunable Pump\Polarization Control\9-19-23 '
                          r'Pre, Post EDFA Pulses\post-EDFA\Tektronix Scope')
+    plot_order = [0, 6, 2, 4, 3, 5, 1]
+    offset = 1.2
     dfs = directory_to_dataframes(directorypath)
     labels = ('Background',
-              'f_rep/4 vertical',
-              'f_rep vertical',
-              'f_rep/2 vertical',
-              'f_rep/2 horizontal',
-              'f_rep/4 horizontal',
-              'f_rep horizontal'
+              r'$f_{rep}/4$ vertical',
+              r'$f_{rep} vertical',
+              r'f_{rep}/2 vertical',
+              r'f_{rep}/2 horizontal',
+              r'f_{rep}/4 horizontal',
+              r'f_{rep} horizontal'
               )
+    data = get_scope_data(dfs, labels)
+    normalize_by_maximum(data, 'voltage_V')
+
+    for i, tup in enumerate(sorted(data.items(), key=lambda x: plot_order[labels.index(x[0])])[3:]):
+        ax[1].plot(tup[1]['time_s'], tup[1]['voltage_V'] - i * offset, label=tup[0])
+    # Add axis labels and a legend
+    ax[1].set_xlabel('Time (s)', fontsize=axes_text_size)
+    ax[1].set_ylabel('Voltage (arb.)', fontsize=axes_text_size)
+    # ax[1].legend(fontsize=legend_text_size)
+    ax[1].set_title(f'Post-EDFA Pulses', fontsize=title_text_size)
+
+    # Begin plot of pre-EDFA pulses
+    directorypath = Path(r'C:\Users\wahlm\Documents\School\Research\Allison\Tunable Pump\Polarization Control\9-19-23 '
+                         r'Pre, Post EDFA Pulses\pre-EDFA\Tektronix Scope')
+    dfs = directory_to_dataframes(directorypath)
+    labels = ('Background',
+              r'$f_{rep}$ horizontal',
+              r'$f_{rep}/2$ horizontal',
+              r'$f_{rep}/4$ horizontal',
+              r'$f_{rep}$ vertical',
+              r'$f_{rep}/2$ vertical',
+              r'$f_{rep}/4$ vertical'
+              )
+    plot_order = [0, 1, 3, 5, 2, 4, 6]
 
     data = get_scope_data(dfs, labels)
-    # normalize_by_maximum(data, 'voltage_V')
-    plot_order = [0, 6, 2, 4, 3, 5, 1]
+    normalize_by_maximum(data, 'voltage_V')
 
-    # Create a figure and axis object using matplotlib
-    fig, ax = plt.subplots(1, 2, figsize=(20, 8))
-    offset = .3
-    for i, tup in enumerate(sorted(data.items(), key=lambda x: plot_order[labels.index(x[0])])):
-        if i > 0:
-            ax[0].plot(tup[1]['time_s'], tup[1]['voltage_V'] - i * offset, label=tup[0])
+    for i, tup in enumerate(sorted(data.items(), key=lambda x: plot_order[labels.index(x[0])])[3:]):
+        ax[0].plot(tup[1]['time_s'], tup[1]['voltage_V'] - i * offset, label=tup[0])
     # Add axis labels and a legend
-    ax[0].set_xlabel('Time [s]')
-    ax[0].set_ylabel('Voltage [V]')
-    ax[0].legend()
-    ax[0].set_title(f'Scope Trace of Pulse Selection, {offset} V Offset')
-
-    directorypath = Path(r'C:\Users\wahlm\Documents\School\Research\Allison\Tunable Pump\Polarization Control\9-19-23 '
-                         r'Pre, Post EDFA Pulses\post-EDFA\Rigol RF Spectrum Analyzer')
-    raw_data = readFromFiles(Path(directorypath), skip_header=2)
-    labels = ('Background',
-              'f_rep horizontal',
-              'f_rep/2 horizontal',
-              'f_rep/4 horizontal',
-              'f_rep/4 vertical',
-              'f_rep/2 vertical',
-              'f_rep vertical'
-              )
-
-    data = [RFSAData(np.array([dat[:, 0], dat[:, 2]]).transpose(), ('Hz', 'dBm'), labels[i], frep_MHz=60.5) for i, dat in enumerate(raw_data)]
-    plot_order = [0, 1, 3, 5, 6, 4, 2]
-    offset = 30
-    for i, datum in enumerate(sorted(data, key=lambda data: plot_order[labels.index(data.label)])):
-        if i > 0:
-            ax[1].plot(datum.x_axis_data, datum.y_axis_data - i * offset, label=datum.label)
-    ax[1].set_xlim([0, 2.5e8])
-    ax[1].set_ylim([-250, -50])
-    ax[1].set_xlabel(f'Frequency ({data[0].x_axis_units})')
-    ax[1].set_ylabel(f'Spectral Power ({data[0].y_axis_units})')
-    ax[1].legend()
-    ax[1].set_title(f'RF Spectrum of Pulse Selection, {offset} dB Offset')
-    fig.canvas.manager.window.showMaximized()  # toggle fullscreen mode
+    ax[0].set_xlabel('Time (s)', fontsize=axes_text_size)
+    ax[0].set_ylabel('Voltage (arb.)', fontsize=axes_text_size)
+    ax[0].legend(fontsize=legend_text_size)
+    ax[0].set_title(f'Pre-EDFA Pulses', fontsize=title_text_size)
+    # fig.canvas.manager.window.showMaximized()  # toggle fullscreen mode
     plt.tight_layout()
     if save_fig:
-        fig.savefig(directorypath.parent / filename, dpi=300)
+        fig.savefig(savepath / filename, dpi=300)
 
     plt.show()
